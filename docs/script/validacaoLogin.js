e8240923b7f420616f6cf4b72a4859aab5a0a412
Pedrix <pedrix.nascimentovr@gmail.com>
2025-11-05 18:09:58 -0300

    Mudanças de Css

--- docs/script/validacaoLogin.js
+++ docs/script/validacaoLogin.js
@@ -7,16 +7,16 @@ const loginSenha = document.getElementById('senha-login');
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if (!loginEmail.value.includes("@")) {
-        alert("Digite um e-mail válido.");
-        return;
-    }
+    // if (!loginEmail.value.includes("@")) {
+    //     alert("Digite um e-mail válido.");
+    //     return;
+    // }
 
-    // CORREÇÃO AQUI: Use loginSenha.value.length
-    if (loginSenha.value.length < 4) { // Note: Deve ser .value.length, não apenas .value
-        alert('A senha deve conter ao menos 4 dígitos');
-        return;
-    }
+    // // CORREÇÃO AQUI: Use loginSenha.value.length
+    // if (loginSenha.value.length < 8) { // Note: Deve ser .value.length, não apenas .value
+    //     alert('A senha deve conter ao menos 8 dígitos');
+    //     return;
+    // }
 
     alert('Conectado com sucesso!');
 });
